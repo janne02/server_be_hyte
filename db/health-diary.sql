4c9f79ed385885b2fdf7af355177beb7dbe78887
janne02 <jannehurskainen02@gmail.com>
2024-02-05 17:05:36 +0200

    week3 fixed

--- db/health-diary.sql
+++ db/health-diary.sql
@@ -24,6 +24,19 @@ CREATE TABLE DiaryEntries (
 
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
+CREATE TABLE HeartRate (
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    HR INT NOT NULL
+);
+CREATE TABLE UserActivities (
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255)NOT NULL,
+    age INT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    activity VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES HeartRate(user_id)
+);
+
 INSERT INTO Users (username, password, email, user_level) VALUES
   ('johndoe', 'temp-pw-1', 'johndoe@example.com', 'regular'),
   ('janedoe', 'temp-pw-2', 'janedoe@example.com', 'admin'),
@@ -36,10 +49,16 @@ INSERT INTO DiaryEntries (user_id, entry_date, mood, weight, sleep_hours, notes,
   (2, '2024-01-10', 'Stressed', 65.0, 7, 'Busy day, a bit stressed out', '2024-01-10 21:00:00');
 
 INSERT INTO UserActivities (user_id, firstname, age, location, activity) VALUES
-  (10, 'Alexey', 28, 'Tirana', 'Lifting heavy'),
-  (11, 'Joonas', 52, 'Helsinki', 'Cycling'),
-  (12, 'Jan', 39, 'Estonia', 'Jogging'),
-  (13, 'Otto', 18, 'Stockholm', 'Cycling');
+  (1, 'Alexey', 28, 'Tirana', 'Lifting heavy'),
+  (2, 'Joonas', 52, 'Helsinki', 'Cycling'),
+  (3, 'Jan', 39, 'Estonia', 'Jogging'),
+  (4, 'Otto', 18, 'Stockholm', 'Cycling');
+-- Inserting Multiple Heart rates
+INSERT INTO HeartRate (user_id, HR) VALUES
+  (1, 68),
+  (2, 75),
+  (3, 82),
+  (4, 91);
 SELECT Users.username, DiaryEntries.entry_date, DiaryEntries.mood, DiaryEntries.notes
   FROM Users, DiaryEntries
   WHERE DiaryEntries.user_id = Users.user_id;
@@ -50,10 +69,15 @@ SELECT Users.username, DiaryEntries.entry_date, DiaryEntries.mood, DiaryEntries.
 SELECT entry_date, mood, sleep_hours FROM DiaryEntries
   JOIN Users ON DiaryEntries.user_id = Users.user_id
   WHERE username = 'johndoe';
+
+
 -- use cases
 -- Query data
 SELECT * FROM UserActivities WHERE location = 'Helsinki';
+--this query retrieves user activities along with corresponding heart rates
+SELECT UserActivities.user_id, UserActivities.firstname, UserActivities.age, UserActivities.location, UserActivities.activity, HeartRate.HR
+  FROM UserActivities INNER JOIN HeartRate ON UserActivities.user_id = HeartRate.user_id
 -- update data
-Update UserActivities SET location = 'Estonia' WHERE firstname = 'Alexey';
+Update UserActivities SET age = 19 WHERE firstname = 'Otto';
 -- delete data
 DELETE FROM UserActivities WHERE activity = 'Jogging';
