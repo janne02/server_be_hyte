c954efa5d9cb8034cdaf0e805fdeeb3df5467826
Janne <jannehurskainen02@gmail.com>
2024-03-15 12:16:35 +0200

    Final

--- src/routes/entry-router.mjs
+++ src/routes/entry-router.mjs
@@ -7,41 +7,184 @@ import {
   deleteEntry,
 } from '../controllers/entry-controller.mjs';
 import {authenticateToken} from '../middlewares/authentication.mjs';
-import {body} from 'express-validator';
+import { body } from 'express-validator';
 
 const entryRouter = express.Router();
 
-entryRouter
-  .route('/')
-  .get(authenticateToken, getEntries)
-  .post(
-    body('entry_date').isISO8601(),
-    body('mood').notEmpty().isString(),
-    body('weight').notEmpty().isNumeric(),
-    body('sleep_hours')
-      .notEmpty()
-      .isFloat({min: 0, max: 24})
-      .withMessage('Sleep hours must be between 0h and 24h.'),
-    body('notes').optional().isString(),
-    authenticateToken,
-    postEntry,
-  );
+/**
+ * @api {get} /entries Retrieve All Entries
+ * @apiVersion 1.0.0
+ * @apiName GetEntries
+ * @apiGroup Entries
+ * @apiPermission token
+ * @apiHeader {String} Authorization Bearer token.
+ *
+ * @apiSuccess {Object[]} entries List of entries.
+ * @apiSuccess {String} entries._id Entry ID.
+ * @apiSuccess {String} entries.entry_date Date of the entry (ISO 8601 format).
+ * @apiSuccess {String} entries.mood Mood of the entry.
+ * @apiSuccess {Number} entries.weight Weight recorded in the entry.
+ * @apiSuccess {Number} entries.sleep_hours Hours of sleep recorded in the entry.
+ * @apiSuccess {String} [entries.notes] Additional notes for the entry (optional).
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {
+ *         "_id": "609bd1586a9c1a0015fbd0bb",
+ *         "entry_date": "2022-05-12T00:00:00.000Z",
+ *         "mood": "Happy",
+ *         "weight": 70,
+ *         "sleep_hours": 8,
+ *         "notes": "Feeling great today!"
+ *       }
+ *     ]
+ *
+ * @apiError UnauthorizedError Authentication token is missing or invalid.
+ */
 
-entryRouter
-  .route('/:id')
+entryRouter.route('/').get(authenticateToken, getEntries)
+
+ /**
+     * @api {post} /entries Create a new Entry
+     * @apiVersion 1.0.0
+     * @apiName PostEntry
+     * @apiGroup Entries
+     * @apiPermission token
+     * @apiHeader {String} Authorization Bearer token.
+     *
+     * @apiParam {String} entry_date Date of the entry (ISO 8601 format).
+     * @apiParam {String} mood Mood of the entry.
+     * @apiParam {Number} weight Weight recorded in the entry.
+     * @apiParam {Number} sleep_hours Hours of sleep recorded in the entry.
+     * @apiParam {String} [notes] Additional notes for the entry (optional).
+     *
+     * @apiSuccess {String} _id Entry ID.
+     * @apiSuccess {String} entry_date Date of the entry (ISO 8601 format).
+     * @apiSuccess {String} mood Mood of the entry.
+     * @apiSuccess {Number} weight Weight recorded in the entry.
+     * @apiSuccess {Number} sleep_hours Hours of sleep recorded in the entry.
+     * @apiSuccess {String} [notes] Additional notes for the entry (optional).
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "_id": "609bd1586a9c1a0015fbd0bb",
+     *       "entry_date": "2022-05-12T00:00:00.000Z",
+     *       "mood": "Happy",
+     *       "weight": 70,
+     *       "sleep_hours": 8,
+     *       "notes": "Feeling great today!"
+     *     }
+     *
+     * @apiError UnauthorizedError Authentication token is missing or invalid.
+     * @apiError ValidationError One or more input parameters are invalid.
+     */
+.post(
+body('entry_date').isISO8601(),
+body('mood').notEmpty().isString(),
+body('weight').notEmpty().isNumeric(),
+body('sleep_hours').notEmpty().isFloat({ min: 0, max: 24 }).withMessage('Sleep hours must be between 0h and 24h.'),
+body('notes').optional().isString(),
+authenticateToken,
+postEntry);
+
+entryRouter.route('/:id')
   .get(getEntryById)
-  .put(
-    body('entry_date').optional().isISO8601(),
-    body('mood').optional().isString(),
-    body('weight').optional().isNumeric(),
-    body('sleep_hours')
-      .optional()
-      .isFloat({min: 0, max: 24})
-      .withMessage('Sleep hours must be between 0h and 24h'),
-    body('notes').optional().isString(),
-    authenticateToken,
-    putEntry,
-  )
+
+  /**
+ * @api {get} /entries/:id Retrieve Entry by ID
+ * @apiVersion 1.0.0
+ * @apiName GetEntryById
+ * @apiGroup Entries
+ *
+ * @apiParam {String} id Entry ID.
+ *
+ * @apiSuccess {String} _id Entry ID.
+ * @apiSuccess {String} entry_date Date of the entry (ISO 8601 format).
+ * @apiSuccess {String} mood Mood of the entry.
+ * @apiSuccess {Number} weight Weight recorded in the entry.
+ * @apiSuccess {Number} sleep_hours Hours of sleep recorded in the entry.
+ * @apiSuccess {String} [notes] Additional notes for the entry (optional).
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "_id": "609bd1586a9c1a0015fbd0bb",
+ *       "entry_date": "2022-05-12T00:00:00.000Z",
+ *       "mood": "Happy",
+ *       "weight": 70,
+ *       "sleep_hours": 8,
+ *       "notes": "Feeling great today!"
+ *     }
+ *
+ * @apiError NotFoundError The entry with the specified ID was not found.
+ */
+  .put(body('entry_date').optional().isISO8601(),
+  body('mood').optional().isString(),
+  body('weight').optional().isNumeric(),
+  body('sleep_hours').optional().isFloat({ min: 0, max: 24 }).withMessage('Sleep hours must be between 0h and 24h'),
+  body('notes').optional().isString(),
+  authenticateToken, putEntry)
+/**
+ * @api {put} /entries/:id Update Entry by ID
+ * @apiVersion 1.0.0
+ * @apiName PutEntry
+ * @apiGroup Entries
+ * @apiPermission token
+ * @apiHeader {String} Authorization Bearer token.
+ *
+ * @apiParam {String} id Entry ID.
+ * @apiParam {String} [entry_date] Date of the entry (ISO 8601 format).
+ * @apiParam {String} [mood] Mood of the entry.
+ * @apiParam {Number} [weight] Weight recorded in the entry.
+ * @apiParam {Number} [sleep_hours] Hours of sleep recorded in the entry.
+ * @apiParam {String} [notes] Additional notes for the entry (optional).
+ *
+ * @apiSuccess {String} _id Entry ID.
+ * @apiSuccess {String} entry_date Date of the entry (ISO 8601 format).
+ * @apiSuccess {String} mood Mood of the entry.
+ * @apiSuccess {Number} weight Weight recorded in the entry.
+ * @apiSuccess {Number} sleep_hours Hours of sleep recorded in the entry.
+ * @apiSuccess {String} [notes] Additional notes for the entry (optional).
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "_id": "609bd1586a9c1a0015fbd0bb",
+ *       "entry_date": "2022-05-12T00:00:00.000Z",
+ *       "mood": "Happy",
+ *       "weight": 70,
+ *       "sleep_hours": 8,
+ *       "notes": "Feeling great today!"
+ *     }
+ *
+ * @apiError UnauthorizedError Authentication token is missing or invalid.
+ * @apiError NotFoundError The entry with the specified ID was not found.
+ * @apiError ValidationError One or more input parameters are invalid.
+ */
+
   .delete(authenticateToken, deleteEntry);
+   /**
+     * @api {delete} /entries/:id Delete Entry by ID
+     * @apiVersion 1.0.0
+     * @apiName DeleteEntry
+     * @apiGroup Entries
+     * @apiPermission token
+     * @apiHeader {String} Authorization Bearer token.
+     *
+     * @apiParam {String} id Entry ID.
+     *
+     * @apiSuccess {String} message Success message.
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "message": "Entry deleted successfully"
+     *     }
+     *
+     * @apiError UnauthorizedError Authentication token is missing or invalid.
+     * @apiError NotFoundError The entry with the specified ID was not found.
+     */
 
 export default entryRouter;
--- src/routes/user-router.mjs
+++ src/routes/user-router.mjs
@@ -1,7 +1,8 @@
 import express from 'express';
-import {body} from 'express-validator';
-import {authenticateToken} from '../middlewares/authentication.mjs';
+import {authenticateToken} from '../middlewares/authentication.mjs'
+import { body } from 'express-validator';
 import {validationErrorHandler} from '../middlewares/error-handler.mjs';
+
 
 import {
   getUserById,
@@ -13,35 +14,201 @@ import {
 
 const userRouter = express.Router();
 
-// /user endpoint
-userRouter
-  .route('/')
-  // list users
+/**
+ * @api {get} /user Get All Users
+ * @apiVersion 1.0.0
+ * @apiName GetUsers
+ * @apiGroup User
+ * @apiPermission token
+ * @apiHeader {String} Authorization Bearer token.
+ *
+ * @apiSuccess {Object[]} users List of users.
+ * @apiSuccess {Number} users.user_id User ID.
+ * @apiSuccess {String} users.username Username of the user.
+ * @apiSuccess {String} users.email Email of the user.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {
+ *         "user_id": 1,
+ *         "username": "john_doe",
+ *         "email": "john@example.com"
+ *       },
+ *       {
+ *         "user_id": 2,
+ *         "username": "jane_doe",
+ *         "email": "jane@example.com"
+ *       }
+ *     ]
+ *
+ * @apiError UnauthorizedError User is not authenticated.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 403 Forbidden
+ *     {
+ *       "message": "Unauthorized"
+ *     }
+ */
+
+
+userRouter.route('/')
   .get(authenticateToken, getUsers)
-  // update user
-  .put(
-    body('username').trim().isLength({min: 3, max: 20}).isAlphanumeric(),
-    body('password').trim().isLength({min: 8, max: 128}),
-    body('email').trim().isEmail(),
-    authenticateToken,
-    validationErrorHandler,
-    putUser,
-  )
-  // user registration
-  .post(
-    body('username').trim().isLength({min: 3, max: 20}).isAlphanumeric(),
-    body('password').trim().isLength({min: 8, max: 128}),
-    body('email').trim().isEmail(),
-    postUser,
-    validationErrorHandler,
+
+
+
+
+/**
+ * @api {put} /user Update User
+ * @apiVersion 1.0.0
+ * @apiName PutUser
+ * @apiGroup User
+ * @apiPermission token
+ * @apiHeader {String} Authorization Bearer token.
+ *
+ * @apiParam {String} username New username of the user.
+ * @apiParam {String} password New password of the user.
+ * @apiParam {String} email New email of the user.
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "username": "new_username",
+ *       "password": "new_password",
+ *       "email": "new_email@example.com"
+ *     }
+ *
+ * @apiSuccess {String} message Success message.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "User updated successfully"
+ *     }
+ *
+ * @apiError UnauthorizedError User is not authenticated.
+ * @apiError ValidationError Validation error occurred.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "message": "Validation error"
+ *     }
+ */
+
+  .put(body('username').trim().isLength({min: 3, max: 20}).isAlphanumeric(),
+  body('password').trim().isLength({min: 8, max: 128}),
+  body('email').trim().isEmail(),authenticateToken, validationErrorHandler, putUser)
+
+
+
+
+
+
+  /**
+ * @api {post} /user Register User
+ * @apiVersion 1.0.0
+ * @apiName PostUser
+ * @apiGroup User
+ * @apiPermission all
+ *
+ * @apiDescription Register a new user.
+ *
+ * @apiParam {String} username Username of the user.
+ * @apiParam {String} password Password of the user.
+ * @apiParam {String} email Email of the user.
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "username": "new_user",
+ *       "password": "password123",
+ *       "email": "new_user@example.com"
+ *     }
+ *
+ * @apiSuccess {String} message Success message.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 201 Created
+ *     {
+ *       "message": "User created successfully"
+ *     }
+ *
+ * @apiError ValidationError Validation error occurred.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "message": "Validation error"
+ *     }
+ */
+  .post(body('username').trim().isLength({min: 3, max: 20}).isAlphanumeric(),
+  body('password').trim().isLength({min: 8, max: 128}),
+  body('email').trim().isEmail(),
+  postUser, validationErrorHandler
   );
 
-// /user/:id endpoint
-userRouter
-  .route('/:id')
-  // get info of a user
+/**
+ * @api {get} /user/:id Get User by ID
+ * @apiVersion 1.0.0
+ * @apiName GetUserById
+ * @apiGroup User
+ * @apiPermission token
+ * @apiHeader {String} Authorization Bearer token.
+ *
+ * @apiParam {Number} id User ID.
+ *
+ * @apiSuccess {Number} user_id User ID.
+ * @apiSuccess {String} username Username of the user.
+ * @apiSuccess {String} email Email of the user.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "user_id": 1,
+ *       "username": "john_doe",
+ *       "email": "john@example.com"
+ *     }
+ *
+ * @apiError UnauthorizedError User is not authenticated.
+ * @apiError NotFoundError User not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "User not found"
+ *     }
+ */
+userRouter.route('/:id')
   .get(authenticateToken, getUserById)
 
+  /**
+ * @api {delete} /user/:id Delete User by ID
+ * @apiVersion 1.0.0
+ * @apiName DeleteUserById
+ * @apiGroup User
+ * @apiPermission token
+ * @apiHeader {String} Authorization Bearer token.
+ *
+ * @apiParam {Number} id User ID.
+ *
+ * @apiSuccess {String} message Success message.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "User deleted successfully"
+ *     }
+ *
+ * @apiError UnauthorizedError User is not authenticated.
+ * @apiError NotFoundError User not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "User not found"
+ *     }
+ */
   .delete(authenticateToken, deleteUser);
+
+
 
 export default userRouter;
