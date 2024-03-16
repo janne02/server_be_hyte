2cff07b83276a996ab1374deb53ccaf5ae63e00d
Janne02 <35040807+janne02@users.noreply.github.com>
2024-03-16 13:20:02 +0200

    Update error-handler.mjs

--- src/middlewares/error-handler.mjs
+++ src/middlewares/error-handler.mjs
@@ -11,13 +11,13 @@ const customError = (message, status, errors) => {
 const notFoundHandler = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   error.status = 404;
-  next(error); // forward error to error handler
+  next(error);
 };
 /**
  * Custom default middleware for handling errors
  */
 const errorHandler = (err, req, res, next) => {
-  res.status(err.status || 500); // default is 500 if err.status is not defined
+  res.status(err.status || 500);
   res.json({
     error: {
       message: err.message,
